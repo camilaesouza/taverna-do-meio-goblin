89684624afb3cd8cf9295a115d64991990c505a9
camilaesouza <camila-emanuele@hotmail.com>
2025-03-28 20:07:07 -0300

    add project

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Bem-vindo à Taverna do Meio Goblin!</h1>
+    <p>Esta é a página inicial do seu site.</p>
+
+    <NuxtLink to="/miniaturas">Ir para os Titans</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ pages/miniaturas/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Pagina para minis </h1>
+  </div>
+</template>
+
+<script setup>
+</script>
