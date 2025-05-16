90b78093489f5ed2dda58e2f41262ff2b1557ffb
camilaesouza <camila-emanuele@hotmail.com>
2025-05-15 21:36:41 -0300

    add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  tailwindcss: {},
-  modules: [
-      '@nuxtjs/tailwindcss',
-      '@pinia/nuxt',
-      'pinia-plugin-persistedstate/nuxt',
-  ]
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    tailwindcss: {},
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+        'pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/sitemap'
+    ],
+    sitemap: {
+        siteUrl: 'https://www.tavernadomeiogoblin.com.br',
+    }
 })
